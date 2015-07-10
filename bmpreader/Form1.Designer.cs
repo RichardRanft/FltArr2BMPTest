d94fb728923b955ad8f6c9d7a6ef4c5fda92fc3a
RichardRanft <vegassparkyrich@gmail.com>
2015-07-10 06:52:31 -0700

    Initial commit

---
+++ bmpreader/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace bmpreader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.btnBrowseInput = new System.Windows.Forms.Button();
+            this.btnBrowseOutput = new System.Windows.Forms.Button();
+            this.tbxOutputFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ofdInfile = new System.Windows.Forms.OpenFileDialog();
+            this.fbdOutfolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.pbxImagePreview = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagePreview)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input File";
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Location = new System.Drawing.Point(12, 25);
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(432, 20);
+            this.tbxInput.TabIndex = 1;
+            // 
+            // btnBrowseInput
+            // 
+            this.btnBrowseInput.Location = new System.Drawing.Point(450, 23);
+            this.btnBrowseInput.Name = "btnBrowseInput";
+            this.btnBrowseInput.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseInput.TabIndex = 2;
+            this.btnBrowseInput.Text = "Browse";
+            this.btnBrowseInput.UseVisualStyleBackColor = true;
+            this.btnBrowseInput.Click += new System.EventHandler(this.btnBrowseInput_Click);
+            // 
+            // btnBrowseOutput
+            // 
+            this.btnBrowseOutput.Location = new System.Drawing.Point(450, 62);
+            this.btnBrowseOutput.Name = "btnBrowseOutput";
+            this.btnBrowseOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseOutput.TabIndex = 5;
+            this.btnBrowseOutput.Text = "Browse";
+            this.btnBrowseOutput.UseVisualStyleBackColor = true;
+            this.btnBrowseOutput.Click += new System.EventHandler(this.btnBrowseOutput_Click);
+            // 
+            // tbxOutputFile
+            // 
+            this.tbxOutputFile.Location = new System.Drawing.Point(12, 64);
+            this.tbxOutputFile.Name = "tbxOutputFile";
+            this.tbxOutputFile.Size = new System.Drawing.Size(432, 20);
+            this.tbxOutputFile.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output File";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(12, 90);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 6;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // pbxImagePreview
+            // 
+            this.pbxImagePreview.Location = new System.Drawing.Point(12, 119);
+            this.pbxImagePreview.Name = "pbxImagePreview";
+            this.pbxImagePreview.Size = new System.Drawing.Size(1024, 1024);
+            this.pbxImagePreview.TabIndex = 7;
+            this.pbxImagePreview.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1048, 1053);
+            this.Controls.Add(this.pbxImagePreview);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnBrowseOutput);
+            this.Controls.Add(this.tbxOutputFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnBrowseInput);
+            this.Controls.Add(this.tbxInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagePreview)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.Button btnBrowseInput;
+        private System.Windows.Forms.Button btnBrowseOutput;
+        private System.Windows.Forms.TextBox tbxOutputFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog ofdInfile;
+        private System.Windows.Forms.FolderBrowserDialog fbdOutfolder;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.PictureBox pbxImagePreview;
+    }
+}
+
---
+++ bmpreader/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bmpreader
+{
+    public partial class Form1 : Form
+    {
+        private float min_val = 1000;
+        private float max_val = -1000;
+        public static Bitmap BytesToBitmap(byte[] byteArray)
+        {
+            using (MemoryStream ms = new MemoryStream(byteArray))
+            {
+                Bitmap img = (Bitmap)Image.FromStream(ms);
+                return img;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void processFile(String infile, String outfile)
+        {
+            if (infile.Length < 1 || outfile.Length < 1)
+                return;
+            FileStream fs = new FileStream(infile, FileMode.Open, FileAccess.Read);
+            BinaryReader r = new BinaryReader(fs);
+            long len = fs.Length / 4;
+            float[] buffer = new float[len];
+            byte[] megagrey = new byte[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                // byte[] myData = new byte[4];
+                //r.Read(myData,0,4);
+                //Single myvalue = BitConverter.ToSingle(myData, 0);
+                buffer[i] = r.ReadSingle();
+                //buffer[i] = 
+                if (min_val > buffer[i])
+                {
+                    min_val = buffer[i];
+                }
+                if (max_val < buffer[i])
+                {
+                    max_val = buffer[i];
+                }
+            }
+            for (int index = 0; index < 1048576; index++)
+            {
+                float max_min = (max_val + min_val);
+                float temp = (buffer[index] + min_val);
+                float div = ((temp / max_min) * 255);
+                //div += 1/2;
+                if (div < 0)
+                {
+                    div = 0;
+                }
+                megagrey[index] = System.Convert.ToByte(div);
+                //megagrey[index] = System.Convert.ToByte(buffer[index]*255);
+            }
+
+            Bitmap newBitmap = new Bitmap(1024, 1024, PixelFormat.Format24bppRgb);
+
+            for (int j = 0; j < 1024; j++)
+            {
+                for (int i = 0; i < 1024; i++)
+                {
+                    Color newColor = Color.FromArgb((int)megagrey[i + j * 1024], (int)megagrey[i + j * 1024], (int)megagrey[i + j * 1024]);
+                    //Color newColor = Color.FromArgb((int)(i/4), (int)(j/4), (int)0);
+                    newBitmap.SetPixel(i, j, newColor);
+                }
+            }
+            pbxImagePreview.Image = newBitmap;
+
+            // MemoryStream bitmapDataStream = new MemoryStream(megagrey);
+            //  Bitmap bitmap = new Bitmap(bitmapDataStream);
+            // bitmap.Save("medota2", ImageFormat.Bmp);
+            Image img = (Image)newBitmap;
+            if (Path.GetFileName(outfile) == "")
+                outfile = outfile + "\\image.bmp";
+            if (!outfile.ToLower().Contains(".bmp"))
+                outfile += ".bmp";
+            newBitmap.Save(outfile, ImageFormat.Bmp);
+            //int[,] var = new int[1024,1024];
+            float[] var = new float[1024];
+            string line;
+            System.IO.StreamWriter file = new System.IO.StreamWriter("Log.txt");
+            for (int j = 0; j < 1024; j++)
+            {
+                for (int i = 0; i < 1024; i++)
+                {
+                    //printf((int)megagrey[i + j * 1024]);
+                    //var[j,i] = ((int)megagrey[i + j * 1024]);
+                    var[i] = ((float)buffer[i + j * 1024]);
+                    //line = System.Convert.ToString(var[i]);
+                    line = var[i].ToString("R");
+                    file.Write(line);
+                    line = " ";
+                    file.Write(line);
+                }
+            }
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            processFile(tbxInput.Text, tbxOutputFile.Text);
+        }
+
+        private void btnBrowseInput_Click(object sender, EventArgs e)
+        {
+            if(ofdInfile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbxInput.Text = ofdInfile.FileName;
+            }
+        }
+
+        private void btnBrowseOutput_Click(object sender, EventArgs e)
+        {
+            if(fbdOutfolder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                tbxOutputFile.Text = fbdOutfolder.SelectedPath + "\\output.bmp";
+            }
+        }
+    }
+}
